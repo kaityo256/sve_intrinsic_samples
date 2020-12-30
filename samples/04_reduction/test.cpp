9655248d2eff8b46f5c72b51cde18d8755b0d292
H. Watanabe <kaityo@users.sourceforge.jp>
2020-12-30 11:55:19 +0000

    adda addv

--- samples/04_reduction/test.cpp
+++ samples/04_reduction/test.cpp
@@ -30,24 +30,43 @@ void show_ppr(svbool_t tp) {
   std::cout << std::endl;
 }
 
-void adda(){
-  int n = svcntd();
-  std::vector<double> a(n);
-  for(int i=0;i<n;i++){
-    a[i] = (i+1)*1e-14;
-  }
-	//std::shuffle(a.begin(), a.end(), std::mt19937());
+void add_vector(){
+  const double a = 1e-8;
+	const double b = 1e8;
+	double d[8] = {a, b, a, b, a, b, a, b};
   svbool_t tp = svptrue_b64();
-  svfloat64_t va = svld1_f64(tp, a.data());
-  std::cout << "va = " << std::endl;
-  svshow(va);
+  svfloat64_t va = svld1_f64(tp, d);
 	float64_t sum = svadda_f64(tp, 0.0, va);
-	printf("sum = %.15f\n",sum);
+	printf("adda = %.15f\n",sum);
+	float64_t sum2 = svaddv_f64(tp, va);
+	printf("addv = %.15f\n",sum2);
+}
+
+void add_scalar(){
+  const double a = 1e-8;
+	const double b = 1e8;
+	double d[8] = {a, b, a, b, a, b, a, b};
+  double sum = 0.0;
+  for (int i = 0; i < 8; i++) {
+    sum += d[i];
+  }
+  printf("adda = %.15f\n", sum);
+  double s1 = d[0] + d[1];
+  double s2 = d[2] + d[3];
+  double s3 = d[4] + d[5];
+  double s4 = d[6] + d[7];
+  double s12 = s1 + s2;
+  double s34 = s3 + s4;
+  double sum2 = s12 + s34;
+  printf("addv = %.15f\n", sum2);
 }
 
 
 int main(){
-  std::cout << "adda" << std::endl;
-  adda();
+  std::cout << "add vector" << std::endl;
+  add_vector();
+  std::cout << std::endl;
+  std::cout << "add scalar" << std::endl;
+  add_scalar();
   std::cout << std::endl;
 }
